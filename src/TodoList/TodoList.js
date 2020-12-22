0564659df6d6845925acb64600a7b2273c44b972
John Obla <john-obla@live.co.uk>
2020-12-22 16:50:35 +0000

    Changed 'name' to 'task'

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import Todo from '../Todo/Todo';
 
 class TodoList extends Component {
+  state = { todos: [{ task: 'Get the dog' }] };
+
+  renderTodoList = () => {};
+
   render() {
     return (
       <div>
